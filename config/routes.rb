c7b26a0e36b071994afd30eb89da6e44ee513245
nathan <nathanshulkin@gmail.com>
2023-02-21 16:50:52 +0000

    adding about me and projects pagez

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  
+  resources :about
+  resources :projects
   root 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
